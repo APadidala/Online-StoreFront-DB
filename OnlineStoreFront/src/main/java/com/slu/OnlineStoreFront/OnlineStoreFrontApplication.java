1dabaa9da2dc2e098e3544db1f94924de8bc401f
Mani Kumar <manikumar.klu@gmail.com>
2023-11-09 23:55:28 -0600

    US 0000

---
+++ OnlineStoreFront/src/main/java/com/slu/OnlineStoreFront/OnlineStoreFrontApplication.java
@@ -0,0 +1,13 @@
+package com.slu.OnlineStoreFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreFrontApplication.class, args);
+	}
+
+}
